0594fa545414c642a146c6c594d81823347af6cb
nour az <naz.199860@gmail.com>
2022-06-06 20:11:34 +0300

    beer

--- first_laravel/app/Http/Controllers/beer.php
+++ first_laravel/app/Http/Controllers/beer.php
@@ -5,6 +5,12 @@ class randomBeer extends Controller{
 function get_random_beer(){
     $link=Http::get('https://api.punkapi.com/v2/beers');
     $data=json_decode($link);
+    $random=rand(0,count($data)-1);
+    return response()->json([
+        "message"=>"success",
+        "beerRecipe"=>$data[$random]
+    ]);
+
 
 
 
--- first_laravel/routes/api.php
+++ first_laravel/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Controllers\pallindrom;
 use Illuminate\Http\Controllers\seconds;
 use Illuminate\Http\Controllers\output_texts;
+use Illuminate\Http\Controllers\beer;
+
 
 
 
@@ -26,5 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/pallindrom',[pallindrom::class,"num_of_pallindroms"]);
 Route::get('/time',[seconds::class, "time_seconds"]);
 Route::get('/text-attachments',[output_texts::class, "text"]);
+Route::get('/text-attachments',[beer::class, "get_random_beer"]);
+
 
 
